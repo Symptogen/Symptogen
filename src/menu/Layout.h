57de4c2fc4bf54243414a52c78a7f076c999174f
Cecilia <cecilia.lejeu@gmail.com>
2014-02-12 21:10:28 +0100

    Added doc : * doc for the Layout class * doc for the LineEdit class * doc for the ManageGamesMenu class

--- src/menu/Layout.h
+++ src/menu/Layout.h
@@ -4,30 +4,53 @@
 #include "GuiComponent.h"
 #include <map>
 
+/** @namespace Symp */
 namespace Symp {
 
+/**
+* @enum LayoutFillAttribute enum 
+* The #LayoutFillAttribute enum presents convenient fields for the choice of the type of #Layout that will be used.
+* The customization of a #Layout can be made following these three types : make a border, or make a background, or
+* do not show the #Layout. When a #Layout is used with the border or background customization type selected, then the
+* unique #Color given in argument is used wether for the border, or the background. Impossible to use both.
+* @see GuiComponent
+* @see Layout
+* @see Color
+*/
 enum LayoutFillAttribute {
-	BORDER,
-	BACKGROUND,
-	NONE
+	BORDER,  /*!< Choose to apply the color of the #Layout to its border */
+	BACKGROUND,  /*!< Choose to apply the color of the #Layout to its background */
+	NONE  /*!< Choose not to show the #Layout */
 };
 
+/**
+* @class Layout class inherits the #GuiComponent class
+* The #Layout class is part of the menu graphical components and can be used only in the menu context. A #Layout is
+* meant to group other #GuiComponents and to organize their display in a regular grid. It is possible to use the #Layout 
+* to group #GuiComponent and to manually set the components position, without organizing them automatically into a grid.
+* A #Layout can be cutomized following the #LayoutFillAttribute. As every #GuiComponent, to be displayed, the #Layout 
+* must be added to the #MenuManager. Moreover, a #Layout automatically add its components to the #MenuManager. A #Layout
+* can contain other #Layout and manage them as if they are simple #GuiComponent.
+* @see MenuManager
+* @see GuiComponent
+* @see LayoutFillAttribute
+* @see Layout()
+* @see ~Layout()
+*/
 class Layout : public GuiComponent {
 public:
 	Layout();
-	Layout(float iPosX, float iPosY, int iWidth, int iHeight, LayoutFillAttribute fillAttribute = LayoutFillAttribute::NONE);
-	Layout(float iPosX, float iPosY, int iWidth, int iHeight, Color color, LayoutFillAttribute fillAttribute = LayoutFillAttribute::NONE);
+	Layout(float iPosX, float iPosY, int iWidth, int iHeight, Color color = Color(0, 0, 0), LayoutFillAttribute fillAttribute = LayoutFillAttribute::NONE);
 	~Layout();
-	void addComponent(GuiComponent* pComponent, int column, int row, bool resizable=true);
+
+	virtual void update(){}
+
+	void addComponent(GuiComponent* pComponent, int column, int row, bool bResizable=true);
 	void fill(Symp::Color color);
 	void insertSpace(int iColumn, int iRow);
 	void computeGrid(int iColumn, int iRow);
 	void resizeComponents();
-	void relocateComponents();
-	GuiComponent* getPreviousRowComponent(int row);
-	GuiComponent* getPreviousColumnComponent(int column);
-	virtual void update(){}
-
+	
 	//Getters
 	int getVerticalMargin() const {return m_iVerticalMargin;}
 	int getHorizontalMargin() const {return m_iHorizontalMargin;}
@@ -39,15 +62,15 @@ public:
 	void setMargins(int iMargin) {m_iHorizontalMargin = m_iVerticalMargin = iMargin;}
 
 private:
-	std::vector<GuiComponent*> m_components;
-	std::map<GuiComponent*, std::pair<int, int> > m_cellsMap;
-	int m_iColumns;
-	int m_iRows;
-	int m_iComponentWidth;
-	int m_iComponentHeight;
-	int m_iHorizontalMargin;
-	int m_iVerticalMargin;
-	Color m_color ;
+	std::vector<GuiComponent*> m_components; /**< the list of #GuiComponents contained into the #Layout */
+	std::map<GuiComponent*, std::pair<int, int> > m_cellsMap; /**< associate a #GuiComponent to its position into the #Layout grid */
+	int m_iColumns; /**< the number of columns that composed the #Layout grid */
+	int m_iRows; /**< the number of rows that composed the #Layout grid */
+	int m_iComponentWidth; /**< the standard width of a #GuiComponent contained in the #Layout grid in pixels */
+	int m_iComponentHeight; /**< the standard width of a #GuiComponent contained in the #Layout grid in pixels */
+	int m_iHorizontalMargin; /**< the horizontal margin between each #GuiComponent in the #Layout grid in pixels */
+	int m_iVerticalMargin; /**< the vertical margin between each #GuiComponent in the #Layout grid in pixels  */
+	Color m_color ; /**< the #Color of the #Layout */
 
 };
 
--- src/menu/LineEdit.h
+++ src/menu/LineEdit.h
@@ -3,14 +3,26 @@
 
 #include "GuiComponent.h"
 
+/** @namespace Symp */
 namespace Symp {
 
+/**
+* @class LineEdit class inherits the #GuiComponent class
+* The #LineEdit class is part of the menu graphical components and can be used only in the menu context. A #LineEdit
+* presents a convenient text input widget that can display the written text, and save it.
+* @todo do the class with last version of Indielib font management
+* @see MenuManager
+* @see GuiComponent
+* @see LineEdit()
+* @see ~LineEdit()
+*/
 class LineEdit : public GuiComponent {
 public:
 	LineEdit(char* text=0);
 	LineEdit(float iPosX, float iPosY, int iWidth, int iHeight, char* text=0);
 	~LineEdit() {}
-	virtual void update();
+
+	virtual void update(){}
 
 };
 
