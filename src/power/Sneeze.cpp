70f3bafa5440c1a6062aca6cf4fe1307e502ae48
BastienLaby <bastien.laby@gmail.com>
2014-02-26 10:23:34 +0100

    Sneeze in the right way

--- src/power/Sneeze.cpp
+++ src/power/Sneeze.cpp
@@ -22,6 +22,9 @@ namespace Symp {
 	void Sneeze::forceExecution() {
 		PhysicalEntity* pDino = EntityManager::getInstance()->getPhysicalDino();
 		float impulse = pDino->getMass() * m_uiRepulsionStrength;
+		if(pDino->getLinearVelocity().x > 0) {
+			impulse = -impulse;
+		}
 		pDino->getb2Body()->ApplyLinearImpulse(b2Vec2(impulse, 0.f), pDino->getb2Body()->GetWorldCenter(), pDino->isAwake());
 		m_uiLastExecution = 0;
 	}
