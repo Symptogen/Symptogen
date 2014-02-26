3aeea09a736896e365891350fd78b73b14d33b6a
Clement <cl.champetier@gmail.com>
2014-02-26 01:38:40 +0100

    Sneeze : launch the power when collision between Dino and any Flower

--- src/physic/ContactListener.cpp
+++ src/physic/ContactListener.cpp
@@ -1,44 +1,55 @@
 #include "ContactListener.h"
 #include "../EntityManager.h"
+#include "../power/Sneeze.h"
 
 namespace Symp {
 	
 void ContactListener::BeginContact(b2Contact* contact) {
 	//check fixture A
+	PhysicalEntity* pPhysicalEntityA;
 	void* userDataA = contact->GetFixtureA()->GetBody()->GetUserData();
 	if(userDataA){
-		PhysicalEntity* pPhysicalEntity = static_cast<PhysicalEntity*>(userDataA);
-		if(pPhysicalEntity){
-			pPhysicalEntity->startContact();
+		pPhysicalEntityA = static_cast<PhysicalEntity*>(userDataA);
+		if(pPhysicalEntityA){
+			pPhysicalEntityA->startContact();
 		}
 	}
 
 	//check fixture B
+	PhysicalEntity* pPhysicalEntityB;
 	void* userDataB = contact->GetFixtureB()->GetBody()->GetUserData();
 	if(userDataB){
-		PhysicalEntity* pPhysicalEntity = static_cast<PhysicalEntity*>(userDataB);
-		if(pPhysicalEntity){
-			pPhysicalEntity->startContact();
+		pPhysicalEntityB = static_cast<PhysicalEntity*>(userDataB);
+		if(pPhysicalEntityB){
+			pPhysicalEntityB->startContact();
 		}
+	}
+
+	//Contact between dino and a flower
+	if((pPhysicalEntityA->getType() == PhysicalType::Dino && pPhysicalEntityB->getType() == PhysicalType::Flower)
+		|| (pPhysicalEntityA->getType() == PhysicalType::Flower && pPhysicalEntityB->getType() == PhysicalType::Dino)){
+		dynamic_cast<Sneeze*>(EntityManager::getInstance()->getPower(PowerType::SneezeType))->forceExecution();
 	}
 }
 
 void ContactListener::EndContact(b2Contact* contact) {
 	//check fixture A
+	PhysicalEntity* pPhysicalEntityA;
 	void* userDataA = contact->GetFixtureA()->GetBody()->GetUserData();
 	if(userDataA){
-		PhysicalEntity* pPhysicalEntity = static_cast<PhysicalEntity*>(userDataA);
-		if(pPhysicalEntity){
-			pPhysicalEntity->endContact();
+		pPhysicalEntityA = static_cast<PhysicalEntity*>(userDataA);
+		if(pPhysicalEntityA){
+			pPhysicalEntityA->endContact();
 		}
 	}
 
 	//check fixture B
+	PhysicalEntity* pPhysicalEntityB;
 	void* userDataB = contact->GetFixtureB()->GetBody()->GetUserData();
 	if(userDataB){
-		PhysicalEntity* pPhysicalEntity = static_cast<PhysicalEntity*>(userDataB);
-		if(pPhysicalEntity){
-			pPhysicalEntity->endContact();
+		pPhysicalEntityB = static_cast<PhysicalEntity*>(userDataB);
+		if(pPhysicalEntityB){
+			pPhysicalEntityB->endContact();
 		}
 	}
 }
